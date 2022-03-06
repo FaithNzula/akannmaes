788040e1ee810a5ab770cbdb43aaa82800d28c45
FaithNzula <faithnzula1230@gmail.com>
2022-03-06 14:06:05 +0300

    footer with flexbox and js for dissabling form submission if there are invalid field entered

---
+++ app.js
@@ -0,0 +1,13 @@
+ // starter JavaScript for disabling form submissions if there are invalid fields
+ (function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.getElementsByClassName('akan-form');
+        // Loop over them and prevent submission
+        const validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
